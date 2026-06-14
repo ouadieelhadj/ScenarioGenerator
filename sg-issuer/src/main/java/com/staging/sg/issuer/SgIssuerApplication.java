a5d6356a6f66163e615568f09693231167693445
Admin <ouadie.wadii@gmail.com>
2026-06-14 12:54:31 +0100

    fix: exclude JWT beans from sg-issuer - add jwt config

--- sg-issuer/src/main/java/com/staging/sg/issuer/SgIssuerApplication.java
+++ sg-issuer/src/main/java/com/staging/sg/issuer/SgIssuerApplication.java
@@ -1,16 +1,24 @@
 package com.staging.sg.issuer;
 
+import com.staging.sg.common.JwtFilter;
+import com.staging.sg.common.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.event.EventListener;
 
-@SpringBootApplication(scanBasePackages = {
-    "com.staging.sg.issuer",
-    "com.staging.sg.common"
-})
+@SpringBootApplication
+@ComponentScan(
+    basePackages = {"com.staging.sg.issuer", "com.staging.sg.common"},
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = {JwtFilter.class, JwtService.class}
+    )
+)
 public class SgIssuerApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SgIssuerApplication.class);
