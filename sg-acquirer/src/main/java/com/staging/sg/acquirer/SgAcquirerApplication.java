ebf5a4bddd28f7a74c5e68e9079b18876b814c1b
Admin <ouadie.wadii@gmail.com>
2026-06-14 13:01:56 +0100

    fix: add EntityScan and EnableJpaRepositories in SgAcquirerApplication and SgIssuerApplication

--- sg-acquirer/src/main/java/com/staging/sg/acquirer/SgAcquirerApplication.java
+++ sg-acquirer/src/main/java/com/staging/sg/acquirer/SgAcquirerApplication.java
@@ -4,12 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {
+@SpringBootApplication
+@ComponentScan(basePackages = {
     "com.staging.sg.acquirer",
     "com.staging.sg.common"
+})
+@EntityScan(basePackages = {
+    "com.staging.sg.common.entity"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.staging.sg.common.repository"
 })
 public class SgAcquirerApplication {
 
--- sg-issuer/src/main/java/com/staging/sg/issuer/SgIssuerApplication.java
+++ sg-issuer/src/main/java/com/staging/sg/issuer/SgIssuerApplication.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(
@@ -19,6 +21,12 @@ import org.springframework.context.event.EventListener;
         classes = {JwtFilter.class, JwtService.class}
     )
 )
+@EntityScan(basePackages = {
+    "com.staging.sg.common.entity"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.staging.sg.common.repository"
+})
 public class SgIssuerApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SgIssuerApplication.class);
